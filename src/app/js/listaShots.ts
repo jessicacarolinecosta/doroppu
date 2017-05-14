33326d99b988fc25407a9ad9a00079e4c7c442fe
jessicacarolinecosta <jessicacosta.dev@hotmail.com>
2017-05-14 20:26:21 -0300

    Listagem de shots com opção de carregar mais

--- src/app/js/listaShots.ts
+++ src/app/js/listaShots.ts
@@ -13,15 +13,29 @@ import 'rxjs/add/operator/map';
   styleUrls: ['../css/listaShots.less']
 })
 export class ListaShots {
-  dadosUsuario: any;
   tokenUsuario: string;
+  dadosUsuario: any;
+  shots: any;
+  filtro: string;
+  paginaShots: number;
+  shotsPorPagina: number;
+  tamanhoGrande: any;
 
   constructor (private http: Http) {
+    this.shots = [];
     this.dadosUsuario = [];
     this.tokenUsuario = '6ade2196ca410421ca4caa7bc18cd31e3822cbc2380393507bc8fda60258114f';
 
     // workaround para não obter erro em console
     this.dadosUsuario.avatar_url = null;
+
+    // valor do filtro para busca de shots (comments, recent ou views)
+    this.filtro = 'recent';
+    this.paginaShots = 1;
+    this.shotsPorPagina = 30;
+
+    // tamanho grande padrão
+    this.tamanhoGrande = true;
   }
 
   // coletando informações do usuário
@@ -34,8 +48,26 @@ export class ListaShots {
         );
   }
 
+  // coletando shots mais recentes
+  public getShots() {
+      this.http.get('https://api.dribbble.com/v1/shots?access_token=' + this.tokenUsuario + '&sort=' + this.filtro + '&page=' + this.paginaShots + '&per_page=' + this.shotsPorPagina).subscribe(
+        data => {
+          // guardando informações coletadas
+          this.shots = this.shots.concat(JSON.parse(data['_body']));
+        }
+      );
+  }
+
+  
+
+  // muda o tamanho da shot baseado no parametro recebido
+  public mudaTamanhoShots(grande) {
+    this.tamanhoGrande = grande;
+  }
+
   // funções executadas ao início
   public ngOnInit() {
       this.getPerfil();
+      this.getShots();
   }
 }
