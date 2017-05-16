2e4cdf1f5a51933cd50771cab5956ad5423077c6
jessicacarolinecosta <jessicacosta.dev@hotmail.com>
2017-05-16 00:47:51 -0300

    Adicionado popup com informações do shot selecionado

--- src/app/js/listaShots.ts
+++ src/app/js/listaShots.ts
@@ -4,6 +4,7 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import {Headers} from '@angular/http';
 
 
 // configurações
@@ -14,15 +15,22 @@ import 'rxjs/add/operator/map';
 })
 export class ListaShots {
   tokenUsuario: string;
-  shots: any;
+  stringBusca: string;
   filtro: string;
+
   paginaShots: number;
   shotsPorPagina: number;
+
+  shots: any;
+  shotDados: any;
   tamanhoGrande: any;
-  stringBusca: string;
+  autorization: any;
+  codigoUsuario: any;
+  jsonHeader: any;
 
   constructor (private http: Http) {
     this.shots = [];
+    this.shotDados = [];
     this.tokenUsuario = '6ade2196ca410421ca4caa7bc18cd31e3822cbc2380393507bc8fda60258114f';
     this.stringBusca = '';
 
@@ -33,7 +41,19 @@ export class ListaShots {
 
     // tamanho grande padrão
     this.tamanhoGrande = true;
-  }
+
+    this.autorization =  { Authorization: 'Bearer 6ade2196ca410421ca4caa7bc18cd31e3822cbc2380393507bc8fda60258114f' };
+
+    // informação do usuario caso autorizado
+    this.codigoUsuario = window.location;
+    this.codigoUsuario = this.codigoUsuario.toString().split("code=");
+    this.codigoUsuario = this.codigoUsuario[1];
+
+    // se o usuário não está autorizado, chama autenticação
+    if(this.codigoUsuario==null || this.codigoUsuario=='') {
+      window.location.href = "https://dribbble.com/oauth/authorize?client_id=cf1d5516801f1146cf1e01760078efbca1daac719a37e8c80c51672e3b1b6b8c&scope=public+write";
+    }
+}
 
   // coletando shots mais recentes
   public getShots() {
@@ -45,6 +65,21 @@ export class ListaShots {
       );
   }
 
+  public popularDados(shot) {
+    // guarda o shot que foi clicado para mostrar a descricao
+    this.shotDados = shot;
+
+    // verifica se o usuário já deu like no shot
+    this.http.get('https://api.dribbble.com/v1/shots/'+ shot.id +'/like?access_token=' + this.tokenUsuario).subscribe(
+      data => {
+        this.shotDados.liked = JSON.parse(data['_body']).id;
+      }
+    );
+
+    // retira as tags em html
+    this.shotDados.description = String(this.shotDados.description).replace(/<[^>]+>/gm, '')
+  }
+
   // muda o tamanho da shot baseado no parametro recebido
   public mudaTamanhoShots(grande) {
     this.tamanhoGrande = grande;
@@ -54,4 +89,33 @@ export class ListaShots {
   public ngOnInit() {
       this.getShots();
   }
+
+  /*** Resolução pendente: CORS ***/
+  public curtirShot(shotId) {
+
+
+   var json = JSON.stringify({ "client_id" : "cf1d5516801f1146cf1e01760078efbca1daac719a37e8c80c51672e3b1b6b8c", "client_secret" : "3bae822e14f0cf450f12928e9fae6218f1c249ccaadd69cee7797cc0646af136", "code" : this.codigoUsuario });
+   var params = 'json=' + json;
+   var headersoptions = new Headers();
+
+
+   headersoptions.append('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
+   headersoptions.append('Access-Control-Expose-Headers', 'ETag, Link, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset');
+   headersoptions.append('Access-Control-Allow-Credentials', 'true');
+   headersoptions.append('Content-Type', 'application/jsonp');
+
+
+  this.http.post('https://dribbble.com/oauth/token/', json, headersoptions).subscribe(data => {
+     alert('ok');
+    }, error => {
+   });
+
+
+   this.http.post('https://api.dribbble.com/v1/shots/'+ shotId +'/like',JSON.stringify({ "client_id" : "cf1d5516801f1146cf1e01760078efbca1daac719a37e8c80c51672e3b1b6b8c", "client_secret" : "3bae822e14f0cf450f12928e9fae6218f1c249ccaadd69cee7797cc0646af136", "code" : this.codigoUsuario }), { headers: this.autorization}).subscribe(data => {
+      alert('ok');
+     }, error => {
+    });
+  }
+
+
 }
